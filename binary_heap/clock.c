1f34cde0054cba87506ea5094d1a1b3e787ed40e
Thomas Phillips <tjp@tjp.id.au>
2017-08-15 02:06:50 +1000

    Fixed up a bunch of timer bugs

--- binary_heap/clock.c
+++ binary_heap/clock.c
@@ -17,20 +17,21 @@
 #include "timer.h"
 #include "beep.h"
 
-#define verbose 5
+#define verbose 0
 #include <sys/debug.h>
 
-#define MAX_TIMERS 1
-
+#define TIMER_INITIAL_CAPACITY 256
 
 typedef struct _timer_struct {
 	struct _timer_struct *next_timer;
 	timer_callback_t callback;
 	void *data;
 	uint64_t delay;
-	int id;
+	uint32_t id;
+	#ifdef DEBUG_TIMER
+	uint32_t debug_cookie;
+	#endif
 }timer_struct;
-
 
 typedef timer_struct * timer_id;
 
@@ -52,6 +53,15 @@ static void print_timer(timer_struct *timer, char *name);
 
 static void fill_list(timer_struct *start, unsigned int num, unsigned int offset);
 
+#ifdef DEBUG_TIMER
+uint32_t DEBUG_COOKIE = 0xb3b3000;
+static inline void update_cookie(void) {
+	for(int i=0;i<node_capacity(beep);++i) {
+		timer_list[i].debug_cookie = DEBUG_COOKIE;
+	}
+}
+#endif
+
 
 void timer_init(void){
 	init_timer_list();
@@ -60,85 +70,122 @@ void timer_init(void){
 }
 
 static void init_timer_list(void){
-	first_free = (timer_struct *)malloc(sizeof(timer_struct) * MAX_TIMERS);
+	first_free = (timer_struct *)malloc(sizeof(timer_struct) * TIMER_INITIAL_CAPACITY);
 	assert(first_free != NULL);
 	timer_list = first_free;
-	fill_list(first_free, MAX_TIMERS, 1);
+	fill_list(first_free, TIMER_INITIAL_CAPACITY, 1);
+#ifdef DEBUG_TIMER
+	update_cookie();
+#endif
 }
 
 
 static void fill_list(timer_struct *start, unsigned int num, unsigned int offset){
-
-	timer_struct *current_timer = &(start[0]);
+	timer_struct* current_timer = &(start[0]);
 	int i = 0;
 	for (i = 0; i < num - 1; ++i) {
 		current_timer->next_timer = &(start[i+1]);
 		current_timer->callback = 0;
 		current_timer->data = 0;
 		current_timer->id = i+offset; // don't have a 0 timer
-
 		current_timer = current_timer->next_timer;
 	}
-
 	current_timer->next_timer = 0;
 	current_timer->callback = 0;
 	current_timer->data = 0;
-	current_timer->id = i+offset; // == MAX_TIMERS
+	current_timer->id = i+offset; // == TIMER_INITIAL_CAPACITY
 }
 
 
 static void init_beep(void){
-	beep = malloc(get_beep_size_for_n_nodes(MAX_TIMERS));
+	beep = malloc(get_beep_size_for_n_nodes(TIMER_INITIAL_CAPACITY));
 	assert(beep != NULL);
-	make_beep(beep, MAX_TIMERS);
+	make_beep(beep, TIMER_INITIAL_CAPACITY);
 }
 
-static void expand(void){
+static int expand(void){
 
 	if (DEBUG) {
-		printf("EXPANDING\n");
+		dprintf(0,"EXPANDING\n");
 	}
 
 	/* Expand the beep */
 	// double the number of nodes we can have
 	unsigned int current_capacity = node_capacity(beep);
 	unsigned int new_capacity = current_capacity*2;
-	beep = realloc(beep, get_beep_size_for_n_nodes(new_capacity));
+	beep_struct* realloced = realloc(beep, get_beep_size_for_n_nodes(new_capacity));
+	if(realloced == NULL) {
+		if(DEBUG) {
+			dprintf(0,"Failed to expand\n");
+		}
+		return 0;
+	}
+	beep = realloced;
+
 	assert(beep != NULL);
 	update_size(beep, new_capacity);
 
 	/* Expand the free list */
 	// realloc the timer list and then update all the pointers in the free list
 	timer_struct *old = timer_list;
-	timer_list = (timer_struct *)realloc(timer_list, sizeof(timer_struct) * new_capacity);
-	assert(timer_list != NULL);
+	timer_struct* temp = realloc(timer_list, sizeof(timer_struct) * new_capacity);
+	if(temp == NULL) {
+		if(DEBUG) {
+			dprintf(0,"Failed to expand\n");
+		}
+		return 0;
+	}
+	timer_list = temp;
+
+#ifdef DEBUG_TIMER
+	DEBUG_COOKIE++;
+#endif
 
 	// we need to fill current_capacity many slots
 	// starting at current_capacity + 1 since the array starts at 1
 	fill_list(&(timer_list[current_capacity]), current_capacity, current_capacity + 1);
 
+	#ifdef DEBUG_TIMER
+		update_cookie();
+	#endif
+
 	// timer_list = first_free;
 	// calculate where we have moved to from the realloc
-	uint64_t diff = timer_list - old;
+	int64_t diff = ((uint8_t*)timer_list) - (uint8_t*) old;
 	// update all the nodes
-	timer_struct *cur = first_free + diff;
+	// Note: In C, pointer arithmatic takes into account the size of the type
+	timer_struct *cur = (timer_struct*) (((uint8_t*)first_free) + diff);
 	while (cur && cur->next_timer) {
-		cur->next_timer += diff;
+		cur->next_timer = (timer_struct*) (((uint8_t*)cur->next_timer) + diff);
+		assert(cur == timer_from_id(cur->id));
 		cur = cur->next_timer;
 	}
 	if (cur) {
-		printf("c\n");
+		dprintf(0,"c\n");
 		cur->next_timer = &(timer_list[current_capacity]);
 		first_free = cur;
 	}else{
 
-		printf("d\n");
+		dprintf(0,"d\n");
 		first_free = &(timer_list[current_capacity]);
 	}
-	printf("ff == %p\n", first_free);
+	dprintf(0,"ff == %p\n", first_free);
+
+	cur = first_free;
+	while(cur) {
+		print_timer(cur, "all free timers");
+		cur = cur->next_timer;
+	}
+
+	return 1;
 }
 
 static void print_timer(timer_struct *timer, char *name){
+#ifdef DEBUG_TIMER
+	dprintf(0, "%s->debug_cookie = %u\n",name,timer->debug_cookie);
+	assert(timer->debug_cookie == DEBUG_COOKIE);
+#endif
+	assert(timer == timer_from_id(timer->id));
 	dprintf(0, "%s = %p\n", name, timer);
 	dprintf(0, "%s->id = %d\n", name, timer->id);
 	dprintf(0, "%s->data = %d\n", name, timer->data);
@@ -148,43 +195,45 @@ static void print_timer(timer_struct *timer, char *name){
 static void print_list(void){
 	for (int i = 0; i < node_capacity(beep); ++i) {
 		/* code */
-		printf("%d\n", timer_list[i].id);
+		dprintf(0,"%d\n", timer_list[i].id);
 	}
 }
 
 static timer_struct *create_timer_add_to_list(uint64_t delay, timer_callback_t callback, void *data){
-	timer_struct *free_timer_struct = first_free;
-	if (free_timer_struct == NULL){
+	if (first_free == NULL){
 		if (DEBUG){
 			dprintf(0,"NO TIMERS LEFT!!\n");
 		}
 		return 0;
 	}
-	timer_struct *next_timer = first_free->next_timer;
-
 
 	print_timer(first_free, "first_free");
-	if (!next_timer){
+	if (!first_free->next_timer){
 		// The list is nearly full, so we should expand
-		expand();
-
-		print_timer(first_free, "first_free");
-		next_timer = first_free->next_timer;
-		free_timer_struct = first_free;
+		if(expand()) {
+			dprintf(0,"Finished expanding\n");
+			print_timer(first_free, "first_free");
+		}
 	}
 
-	first_free = next_timer;
+	timer_struct* free_timer_struct = first_free;
+	first_free = first_free->next_timer;
 	
 	print_timer(first_free, "first_free");
 
+	assert(free_timer_struct->data == 0);
 	free_timer_struct->delay = delay;
 	free_timer_struct->callback = callback;
 	free_timer_struct->data = data;
 	free_timer_struct->next_timer = 0;
 
+#ifdef DEBUG_TIMER
 	print_list();
 	print_timer(free_timer_struct, "free_timer_struct");
 	print_timer(timer_from_id(free_timer_struct->id), "id_timer");
+	assert(free_timer_struct->debug_cookie == DEBUG_COOKIE);
+	assert(timer_from_id(free_timer_struct->id)->debug_cookie == DEBUG_COOKIE);
+#endif
 	assert(free_timer_struct == timer_from_id(free_timer_struct->id));
 
 	return free_timer_struct;
@@ -245,6 +294,9 @@ uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data){
 
 
 static void free_timer(timer_struct * timer){
+#ifdef DEBUG_TIMER
+	assert(timer->debug_cookie == DEBUG_COOKIE);
+#endif
 	timer->next_timer = first_free;
 	first_free = timer;
 	timer->callback = 0;
@@ -297,6 +349,8 @@ int timer_interrupt(void){
 		}else{
 			del_count--;
 		}
+		/* Bugfix: If the timer list is expanded during the callback, this pointer may not be valid */
+		timer = timer_from_id(id);
 		free_timer(timer);
 	}
 
